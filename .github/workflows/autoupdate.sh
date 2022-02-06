ed9a094729704e0719916d7257dc47918bd23407
Aris Paschalidis <aris.paschalidis@gmail.com>
2022-02-06 18:12:15 -0500

    Add workflow to autoupdate example hooks

---
+++ .github/workflows/autoupdate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Rename global pre-commit config so we don't override it
+mv .pre-commit-config.yaml .global.yaml
+
+# Update each example config
+for file in examples/.*.yaml; do
+  echo "Updating ${file} hooks..."
+
+  cp ${file} .pre-commit-config.yaml
+	pre-commit autoupdate
+	cp .pre-commit-config.yaml ${file}
+done
+
+# Restore global pre-commit config
+mv -f .global.yaml .pre-commit-config.yaml
